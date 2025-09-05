fb0229431266b2644e1d05d9c35ed80791a3cacc
桐谷翔 <lionactheart007@gmail.com>
2025-09-05 12:03:54 +0900

    ゴールの作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static string gameState; //Ã“Iƒƒ“ƒo
+
+    void Awake()
+    {
+        //ƒQ[ƒ€‚Ì‰Šúó‘Ô‚ğPlaying‚Æİ’è
+        gameState = "Playing";
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
